a9ee14799b8e272d1ea5cb97678493c5b5d96537
QIQIYAA37 <yf20051010@163.com>
2025-02-20 16:18:48 +0800

    ex2_MusicBox

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,11 @@ public class Main{
          * 
          * 
         */
-        
+        MusicBox mbox2 = new MusicBox("S34TG66", 'X', "Raindrops Remix", "Misty", 
+        "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
+
+        System.out.println("Now playing the song");
+        mbox2.playSong(mbox2.getSongID(), mbox2.getPremiumSong(), 2);
 
     }
 }
--- src/MusicBox.java
+++ src/MusicBox.java
@@ -17,8 +17,12 @@ public class MusicBox {
         this.songID = songID;
         this.isSongPremium = premiumSong;
         this.songArtists = artists;
-        
-        // -- TODO
+        this.songTitle = title;
+        this.songAlbum = Album;
+        this.songGenre = genre;
+        this.songProducer = producer;
+        this.songMusicLabel = label;
+        // -- TODO Finish
         /* Initialise  the rest of the variables in this Constructor block*/
     }
     
@@ -28,15 +32,76 @@ public class MusicBox {
         return songID;
     }
 
-    public String getSongArtists() {
-        return songArtists;
-    }
-
     public char getPremiumSong() {
         return isSongPremium;
     }
 
-    // -- TODO
+    public String getSongTitle() {
+        return songTitle;
+    }
+
+    public String getSongArtists() {
+        return songArtists;
+    }
+
+    public String getSongAlbum() {
+        return songAlbum;
+    }
+
+    public String getSongGenre() {
+        return songGenre;
+    }
+
+    public String getSongProducer() {
+        return songProducer;
+    }
+
+    public String getSongMusicLabel() {
+        return songMusicLabel;
+    }
+
+    public int getNoAds() {
+        return noAds;
+    }
+
+    public void setSongID(String songID) {
+        this.songID = songID;
+    }
+
+    public void setPremiumSong(char premiumSong) {
+        this.isSongPremium = premiumSong;
+    }
+
+    public void setSongTitle(String songTitle) {
+        this.songTitle = songTitle;
+    }
+
+    public void setSongArtists(String songArtists) {
+        this.songArtists = songArtists;
+    }
+
+    public void setSongAlbum(String songAlbum) {
+        this.songAlbum = songAlbum;
+    }
+
+    public void setSongGenre(String songGenre) {
+        this.songGenre = songGenre;
+    }
+
+    public void setSongProducer(String songProducer) {
+        this.songProducer = songProducer;
+    }
+
+    public void setSongMusicLabel(String songMusicLabel) {
+        this.songMusicLabel = songMusicLabel;
+    }
+
+    public void setNoAds(int noAds) {
+        this.noAds = noAds;
+    }
+
+
+    // -- TODO Finish
         /* Create the rest of "getters" for the remaining variables */
     
         /* The following method is complete. You don't have to do anything here. Just try to understand it. */
